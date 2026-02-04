f7b54c2cd74d5681c06f8110fedace68aa1feac1
Callum Taylor <150924494+Kaiyumari15@users.noreply.github.com>
2026-02-04 02:31:40 +0000

    More adjustments for Linux compatibility
    - Adjust sqlite3 library features to include the tokio runtime
    - Update main.rs to listen on 0.0.0.0:3000

--- src/main.rs
+++ src/main.rs
@@ -193,8 +193,8 @@ async fn main() {
         )
         .with_state(app_state);
 
-    println!("Server running on http://localhost:3000");
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    println!("Server running on http://0.0.0.0:3000");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
     axum::serve(listener, app).await.unwrap();
