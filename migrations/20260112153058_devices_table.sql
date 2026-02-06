f1e0162648680ccca4c2d4b6d0a3726bf89e0e09
Callum Taylor <150924494+Kaiyumari15@users.noreply.github.com>
2026-02-06 21:49:33 +0000

    Update database migration for devices table

--- migrations/20260112153058_devices_table.sql
+++ migrations/20260112153058_devices_table.sql
@@ -3,7 +3,6 @@ CREATE TABLE devices (
     id INTEGER PRIMARY KEY,
     node_id INTEGER NOT NULL,
     endpoint_id INTEGER NOT NULL,
-    device_type VARCHAR(50) NOT NULL,
     name VARCHAR(100) NOT NULL,
     capabilities TEXT NOT NULL DEFAULT '{}'
 );
